3a59d8c34c399c66110322455a6d9d4f7c0bf160
Slawomir Kussowski <slawomirkussowski@gmail.com>
2022-04-15 16:11:07 +0200

    added routing for details and page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PokemonsComponent
+  },
+  {
+    path: 'pokemon/:id',
+    component: PokemonDetailsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppCommonModule } from 'projects/common/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonsComponent
+    PokemonsComponent,
+    PokemonDetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
