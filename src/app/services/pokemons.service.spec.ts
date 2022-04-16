4a7f98cea4ae04600f5b81451e91d93c6b930e09
Slawomir Kussowski <slawomirkussowski@gmail.com>
2022-04-16 12:23:08 +0200

    added simple test for data

--- src/app/services/pokemons.service.spec.ts
+++ src/app/services/pokemons.service.spec.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, flushMicrotasks, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { PokemonsService } from './pokemons.service';
 
 
 class HttpClientMock {
   get() {
-    return of({});
+    return of({
+      conunt: 100,
+      next: '',
+      previous: null,
+      results: [
+        { url: 'test1', name: 'test1' },
+        { url: 'test2', name: 'test2' },
+        { url: 'test3', name: 'test3' }
+      ]
+    });
   }
 }
 
@@ -23,4 +32,12 @@ describe('PokemonsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return data'), fakeAsync(() => {
+    let count: number | undefined;
+    service.fetchAll().subscribe(val => count = val.length);
+    flushMicrotasks();
+    expect(count).toBe(3);
+  });
+
 });
